817de22271a3cae133b4e239cdc2c649f54cadb7
altffour <56314286+realaltffour@users.noreply.github.com>
2020-01-12 08:16:48 +0300

    Add APPE command.

--- src/lib/server_pi.rs
+++ src/lib/server_pi.rs
@@ -46,10 +46,36 @@ pub mod server_pi {
                 "STOR" => process_stor_cmd(&mut _stream, &mut _user, &_cmd)?,
                 "ABOR" => process_abor_cmd(&mut _stream, &mut _user, &_cmd)?,
                 "ALLO" => process_allo_cmd(&mut _stream, &mut _user, &_cmd)?,
+                "APPE" => process_appe_cmd(&mut _stream, &mut _user, &_cmd)?,
                 _ => { 
                     ftp::send_reply(&mut _stream, &ftp::reply::COMMAND_NOT_IMPLEMENTED.to_string(), "Command Not Implemented.")?;
                 }
             }
+            return Ok(());
+    }
+
+    pub fn process_appe_cmd(mut _stream: &mut TcpStream, mut _user: &mut ClientConnection, _cmd: &FtpCmd) ->
+        Result<(), Box<dyn std::error::Error>> {
+            // Open Data connection.
+            if _user.connect_mode == FTPModes::Active {
+                // Open data connection.
+                let address = &mut _user.data_ip;
+                address.push_str(":");
+                address.push_str(_user.data_port.to_string().as_str());
+                _user.data_conc = TcpBuilder::new_v4().unwrap().reuse_address(true).unwrap().bind("0.0.0.0:20").unwrap().connect(address.as_str()).unwrap();
+            }
+
+            ftp::send_reply(&mut _stream, &ftp::reply::ABOUT_TO_SEND.to_string(), "Open data channel for file upload.")?;
+
+            // Read all data.
+            let mut buf = vec![];
+            _user.data_conc.read_to_end(&mut buf)?;
+
+            // Store all data.
+            let mut file = OpenOptions::new().create(true).write(true).append(true).open(&ftp::make_path_jailed(&_cmd._args))?;
+            file.write_all(&buf)?;
+
+            ftp::send_reply(&mut _stream, &ftp::reply::CLOSING_DATA_CONNECTION.to_string(), "Successfully transferred.")?;
             return Ok(());
     }
 
@@ -87,6 +113,7 @@ pub mod server_pi {
             file.write_all(&buf)?;
 
             ftp::send_reply(&mut _stream, &ftp::reply::CLOSING_DATA_CONNECTION.to_string(), "Successfully transferred.")?;
+
             return Ok(());
     }
 
