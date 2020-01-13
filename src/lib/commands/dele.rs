1659b99f3b3ec8f1e38537ac1f1bb4aadcf8433a
altffour <56314286+realaltffour@users.noreply.github.com>
2020-01-13 14:36:11 +0300

    DELE now complies with fake chroot.

--- src/lib/commands/dele.rs
+++ src/lib/commands/dele.rs
@@ -12,13 +12,22 @@ Result<(), Box<dyn std::error::Error>> {
         _user.user.rights == db::Rights::Read {
         ftp::send_reply(&mut _stream, 
             &ftp::reply::NOT_AVAILABLE.to_string(), 
-            "You don't have permission to do that.")?;       
+            "You don't have permission to do that.")?;
         return Ok(());
     }
 
-    std::fs::remove_file(&ftp::make_path_jailed(&_cmd._args))?;
-    ftp::send_reply(&mut _stream, 
-        &ftp::reply::REQUESTED_FILE_ACTION_OK.to_string(), 
-        "File deleted successfully.")?;
-    return Ok(());
+    match std::fs::remove_file(&ftp::make_path_jailed(&_cmd._args)) {
+        Ok(_v) => {
+            ftp::send_reply(&mut _stream, 
+                &ftp::reply::REQUESTED_FILE_ACTION_OK.to_string(), 
+                "File deleted successfully.")?;
+        },
+        Err(_e) => {
+            ftp::send_reply(&mut _stream, 
+                &ftp::reply::REQUESTED_ACTION_NOT_TAKEN.to_string(), 
+                "Couldn't delete file.")?;
+        }
+    }
+
+        return Ok(());
 }
