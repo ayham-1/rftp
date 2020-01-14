9dee7225c62bf2287efd14e716828e299d661cf3
altffour <56314286+realaltffour@users.noreply.github.com>
2020-01-14 08:44:11 +0300

    Add MKD command.

---
+++ src/lib/commands/mkd.rs
@@ -0,0 +1,33 @@
+use std::net::TcpStream;
+use crate::ftp::*;
+use crate::defines::defines::*;
+use std::env;
+use std::path::Path;
+
+pub fn cmd(mut _stream: &mut TcpStream, 
+    mut _user: &mut ClientConnection, _cmd: &FtpCmd) ->
+Result<(), Box<dyn std::error::Error>> {
+    // Check for permissions.
+    if _user.user.rights == Rights::Nothing ||
+        _user.user.rights == Rights::List ||
+        _user.user.rights == Rights::Read {
+        ftp::send_reply(&mut _stream, 
+            &ftp::reply::REQUESTED_ACTION_NOT_TAKEN.to_string(), 
+            "You don't have permission to do that.")?;
+        return Ok(());
+    }
+
+    match std::fs::create_dir_all(&ftp::make_path_jailed(&_cmd._args)) {
+        Ok(_v) => {
+            ftp::send_reply(&mut _stream, 
+                &ftp::reply::REQUESTED_FILE_ACTION_OK.to_string(), 
+                "MKD Command Successful.")?;
+        },
+        Err(_e) => {
+            ftp::send_reply(&mut _stream, 
+                &ftp::reply::FILE_UNAVAILABLE.to_string(),
+                "MKD Command Failed.")?;
+        }
+    }
+    return Ok(());
+}
