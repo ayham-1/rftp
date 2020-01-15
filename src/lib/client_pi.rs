38637929522c04bb39973048b08fc053ced26960
altffour <56314286+realaltffour@users.noreply.github.com>
2020-01-15 14:28:20 +0300

    Add pre-match checks.

--- src/lib/client_pi.rs
+++ src/lib/client_pi.rs
@@ -2,7 +2,7 @@ pub mod client_pi {
     use std::net::TcpStream;
     use std::error;
     use std::fmt;
-    
+
     type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
     #[derive(Debug, Clone)]
@@ -19,13 +19,20 @@ pub mod client_pi {
         }
     }
 
-    pub fn send_cmd(mut _stream: &mut TcpStream, _cmd: &str) -> 
+    pub fn send_cmd(mut _stream: &mut TcpStream, mut _cmd: &str) -> 
         Result<()> {
-        match _cmd {
-            _ => {
-                return Err(Box::new(UnrecognizedCmd));
-            }
-        };
-        return Ok(());
+            // Pre-checks.
+            
+            // uppercase all.
+            let uppercmd = _cmd.to_string().to_uppercase().to_owned();
+            let cmd = uppercmd.as_str();
+
+            // Despatch commands.
+            match cmd {
+                _ => {
+                    return Err(Box::new(UnrecognizedCmd));
+                }
+            };
+            return Ok(());
     }
 }
